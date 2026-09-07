8c36f07f82783b321a237694322011abe700f2bf
Shadma Yunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-07 21:37:49 +0530

    Add variables practise

---
+++ Basics/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main (String[] args){
+        int a= 14;
+        char b='A';
+        String c="heeyloo";
+        double height=4.234;
+        boolean isStudent = false;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(height);
+        System.out.println(isStudent);
+    }
+
+    
+
+}
