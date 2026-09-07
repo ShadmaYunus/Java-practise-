0581f6e37db6be85c993775b17cdaab319c4f800
Shadma Yunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-08 01:56:56 +0530

    Add Typecsting practise

---
+++ Basics/Typecasting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Typecasting {
+    public static void main (String[] args) {
+        int a = 23;
+        double b = (double)a;
+
+        System.out.println("The original number is: "+ a);
+        System.out.println("The typecasted value is: "+ b);
+
+    }
+}
