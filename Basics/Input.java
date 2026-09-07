bf53b7340b0eb898342b24f67b601751dd9e06b1
Shadma Yunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-07 22:33:17 +0530

    Add user unput practise

---
+++ Basics/Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Input {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name : ");
+        String name=sc.nextLine();
+        System.out.print("Enter your age");
+        int age=sc.nextInt();
+        System.out.println("hello"+name);
+        System.out.println("Your age is"+age);
+        sc.close();
+    }}
