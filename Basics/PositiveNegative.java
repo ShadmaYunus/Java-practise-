ab98075e020ecddd57fa8a36c921b4fc91a148ff
Shadma Yunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-09 22:11:57 +0530

    Add positive negative

---
+++ Basics/PositiveNegative.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PositiveNegative{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the numbahh");
+        int num = sc.nextInt();
+        if(num>=0){
+            System.out.println("number positive");
+        }
+        else{
+            System.out.println("number negatihvv");
+        }
+        
+
+
+    }
+}
