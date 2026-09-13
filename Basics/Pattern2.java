80335762fad175213b8869bb96858da8425636e6
Shadma Yunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-13 21:53:37 +0530

    Add pattern of numbers

---
+++ Basics/Pattern2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pattern2{
+    public static void main(String [] args){
+        int i;
+        int j;
+        for(i=1; i<=4; i++){
+            for(j=1;j<=i ; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+
+
+        }
+    }
+}
