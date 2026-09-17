96ee4ec3562ad2a07e873c57f7623f3512da8587
Shadma Yunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-17 16:41:14 +0530

    Add function to add with condition

---
+++ Basics/functions.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class functions {
+    static int logic(int x, int y){
+        int z;
+        if(x>y){
+            z= x+y;
+        }
+        else{
+            z = (x+y)*5;
+        }
+        return z;
+    }
+    public static void main(String[] args){
+        int a = 4;
+        int b= 3;
+        int c = logic(a,b);
+        System.out.println(c);
+
+    }
+}
