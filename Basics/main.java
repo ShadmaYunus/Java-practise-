2b3cab582b29d126534383c9fc064d5bcbeec65e
ShadmaYunus <193724326+beelzeweb@users.noreply.github.com>
2026-09-23 23:55:09 +0530

    Function that i was strugglin' to chommittt

---
+++ Basics/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String [] args){
+        int i; int j;
+        for(i = 1 ; i<=4 ; i++){
+            for (j = 1;j<=5 ; j++){
+            System.out.print("*"); 
+        }
+        System.out.println();
+    }
+}
+}
